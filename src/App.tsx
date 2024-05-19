b75cc7822dc7e462de77059257853ac8424e5015
oumaimagd <oumaimagdiri98@gmail.com>
2024-05-19 11:51:27 +0100

    typescript commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 
-function App() {
+import Car from "./components/Car";
+
+const App: React.FC = () => {
+  useEffect(() => {
+    const myCar = new Car("Toyota", "Corolla", 2021);
+
+    myCar.start();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Open the console to see the car engine start message.</p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
